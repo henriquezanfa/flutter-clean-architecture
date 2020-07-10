058ea792132a29a0dcf6c911b7f895437ecd7309
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-07-10 11:51:51 -0300

    Testing and implementing getLastNumberTrivia()

--- lib/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart
@@ -1,6 +1,35 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/error/exceptions.dart';
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString != null)
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    else
+      throw CacheException();
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+    throw UnimplementedError();
+  }
 }
