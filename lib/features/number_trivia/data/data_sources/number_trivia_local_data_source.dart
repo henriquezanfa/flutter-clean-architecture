e4f48a0084553fc7b67c24c7dad3b711b1fc46c2
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-07-10 10:42:48 -0300

    Creating contracts for data sources

---
+++ lib/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart
@@ -0,0 +1,6 @@
+import '../models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  Future<NumberTriviaModel> getLastNumberTrivia();
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
---
+++ lib/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart
@@ -0,0 +1,6 @@
+import '../../domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  Future<NumberTrivia> getConcreteNumberTrivia(int number);
+  Future<NumberTrivia> getRandomNumberTrivia();
+}
